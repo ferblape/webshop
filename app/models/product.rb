88c8de6791ea993a2c6699eb3766c7835e231e4a
arctarus <arctarusnet@gmail.com>
2012-08-15 19:38:16 +0200

    adding validations to Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ActiveRecord::Base
   attr_accessible :brand, :category, :description, :name, :price
+
+  validates :name,  presence: true,
+                    length: { minimum: 3 }
+  validates :price, presence: true,
+                    numericality: true
 end
