b9b92146421c19dec04dee97619dc3ef67973c25
arctarus <arctarusnet@gmail.com>
2012-08-15 19:46:08 +0200

    adding a route for reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Webshop::Application.routes.draw do
-  resources :products
+  resources :products do
+    resources :reviews
+  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
