7be702e163ce9447dd384d0ebfd9e880855d34dd
arctarus <arctarusnet@gmail.com>
2012-08-15 19:43:43 +0200

    added relationship has many between product and review

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ActiveRecord::Base
   attr_accessible :brand, :category, :description, :name, :price
 
+  has_many :reviews
+
   validates :name,  presence: true,
                     length: { minimum: 3 }
   validates :price, presence: true,
