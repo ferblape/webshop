e86d133be00c788ce8af7b0b38cfe06a1a254b1b
arctarus <arctarusnet@gmail.com>
2012-08-15 20:15:12 +0200

    added list of reviews to product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,7 +14,6 @@ class ProductsController < ApplicationController
   # GET /products/1.json
   def show
     @product = Product.find(params[:id])
-    @review = @product.reviews.build
 
     respond_to do |format|
       format.html # show.html.erb
