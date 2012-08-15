69b662b01becf4db92ced5689d4489cde94c5e15
arctarus <arctarusnet@gmail.com>
2012-08-15 20:21:16 +0200

    deleting reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,4 +11,12 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def destroy
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.find(params[:id])
+
+    @review.destroy
+    redirect_to @product
+  end
+
 end
