38a29343d7203d6f54f0e1e92492ed43e6e8358c
arctarus <arctarusnet@gmail.com>
2012-08-15 19:23:51 +0200

    rails autogenerate app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webshop::Application.config.secret_token = 'f3825cf9f6e8ef74c4781fb9b40e6b1c52be2c188668b7a97e0966a83d91549682687bd3e572dd486f18e0cb16cd8e8edbcbf26382b640a31fe9eb4e5b8320c3'
