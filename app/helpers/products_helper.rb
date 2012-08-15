96ceafd4acfb8ef3835ef3db7af189c93a893bc9
arctarus <arctarusnet@gmail.com>
2012-08-15 20:50:54 +0200

    created some helper methods

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,6 @@
 module ProductsHelper
+
+  def tags_names(product)
+    product.tags.map(&:name).join(', ')
+  end
 end
--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,7 @@
 module ReviewsHelper
+
+  def rating_options_for_select
+    (1..5).map {|n| [n,n] }
+  end
+
 end
