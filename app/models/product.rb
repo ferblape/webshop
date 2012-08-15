7258c097b9d625c2c559fc45b70fef937e707964
arctarus <arctarusnet@gmail.com>
2012-08-15 20:22:19 +0200

    Deleting reviews associated with a product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ActiveRecord::Base
   attr_accessible :brand, :category, :description, :name, :price
 
-  has_many :reviews
+  has_many :reviews, :dependent => :destroy
 
   validates :name,  presence: true,
                     length: { minimum: 3 }
